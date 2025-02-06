dfab9b437759da66a53f973e3606b4398934b86c
HanniIsMyGF <kamito.kigari@gmail.com>
2025-02-06 12:24:51 +0700

    Update ser.js

--- ser.js
+++ ser.js
@@ -10,15 +10,23 @@ const todoList = [
 
 app.get('/',(req,res) => res.send("Hello My server"))
 //ข้อ 1
-// app.get('/todos',(req,res) => {
-//     res.json(todoList)
-// })
+app.get('/todos',(req,res) => {
+    res.json(todoList)
+})
 //ข้อ 2
 app.get('/todos/:id' , (req,res) => {
     const data = todoList.find((i) => i.id == req.params.id)
     if (data == undefined) {
         res.json({"message": "ไม่พบบันทึกที่ระบุ"})
     } 
+})
+
+//ข้อ 3
+app.post("/todos", (req,res) => {
+    let id = todoList[todoList.length - 1].id +1
+    let task = req.body.task
+    let comleted = req.body.comleted
+    todoList = [...todoList, {id, task, comleted}]
 })
 
 app.listen(port, () => {
